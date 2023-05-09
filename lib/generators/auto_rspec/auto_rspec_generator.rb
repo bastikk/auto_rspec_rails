fb291d9a2e38fa0f7eec1fee93bf6ab64d6ef2ba
Ivan_Bastylo <b15042002@gmail.com>
2023-05-10 02:14:22 +0300

    Made filepath parsing and generation

--- lib/generators/auto_rspec/auto_rspec_generator.rb
+++ lib/generators/auto_rspec/auto_rspec_generator.rb
@@ -1,27 +1,21 @@
-class AutoRspecGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
+module ModelParser
+  #   read about services
 
-  # argument :file_names, type: :array, default: [], banner: "files names"
+  def parse_relations(file_path)
+    file = File.open(file_path, "r")
+    content = file.read
+    file.close
+    # binding.pry
+    # content.scan(/^ *(belongs_to.*|has_one.*)\r/).uniq
 
-  def create_service_file
-    p 11111
-    p file_name
-    # return
-
-    generator_dir_path = "spec/"
-    #file name make array
-    # parse line into dirs
-    # generator_dir_path = service_dir_path + ("/#{@module_name.underscore}" if @module_name.present?).to_s
-    generator_path = generator_dir_path + "/#{file_name}_spec.rb"
-
-    #make dir path generation
-    Dir.mkdir(generator_dir_path) unless File.exist?(generator_dir_path)
-
-    #parsing file
-    @relations = parse_relations('app/models/'+file_name+'.rb')
-
-    #add check
-    template "model.erb", generator_path
+    # add another checks
+    # add parameters check
+    {
+      belongs_to: parse_belongs_to(content),
+      has_many: parse_has_many(content),
+      has_one: parse_has_one(content),
+      has_and_belongs_to_many: parse_has_and_belongs_to_many(content)
+    }
   end
 
   private
@@ -41,21 +35,54 @@ class AutoRspecGenerator < Rails::Generators::NamedBase
   def parse_has_and_belongs_to_many(content)
     content.scan(/has_and_belongs_to_many\s:(\w+)/).flatten
   end
+end
 
-  def parse_relations(file_path)
-    file = File.open(file_path, "r")
-    content = file.read
-    file.close
-    # binding.pry
-    # content.scan(/^ *(belongs_to.*|has_one.*)\r/).uniq
+class AutoRspecGenerator < Rails::Generators::NamedBase
+  # todo temporary
+  require 'pry'
+  source_root File.expand_path('../templates', __FILE__)
+  include ModelParser
 
-    #add another checks
-    # add parameters check
-    {
-      belongs_to: parse_belongs_to(content),
-      has_many: parse_has_many(content),
-      has_one: parse_has_one(content),
-      has_and_belongs_to_many: parse_has_and_belongs_to_many(content)
-    }
+  def create_service_file
+    # error message if not appropriate path provided
+    unless File.exist?(file_path)
+      puts "#{file_path} doesn't exist!"
+      return
+    end
+
+    directories = File.dirname(file_path).split('/').drop(1)
+    spec_path = 'spec/' + directories.join('/') + "/#{File.basename(file_name, '.*')}_spec.rb"
+
+    # parse classes
+    @class_name = class_name.split('.').first
+    while !(Object.const_defined?(@class_name) || @class_name.blank?)
+      @class_name = @class_name.split('::').drop(1).join('::')
+    end
+    the_class = Module.const_get(@class_name) unless @class_name.blank?
+
+    if the_class < ApplicationRecord
+      create_spec_directory(directories)
+      @relations = parse_relations(file_path)
+      #  todo add
+      template "application_record.erb", spec_path
+    elsif the_class < ActiveModel
+      create_spec_directory(directories)
+      p 'not implemented yet'
+      #   todo add
+    else
+      puts "Spec generation for provided file doesn't supported. Please use it only for ApplicationRecord/ActiveModel classes"
+      return
+    end
+  end
+
+  private
+
+  # Creates spec directory if it doesn't exist
+  def create_spec_directory(directories)
+    spec_dir_path = 'spec/'
+    directories.each do |directory|
+      spec_dir_path += "#{directory}/"
+      Dir.mkdir(spec_dir_path) unless File.exist?(spec_dir_path)
+    end
   end
 end
---
+++ lib/generators/auto_rspec/parsers/model_parser.rb
@@ -0,0 +1,39 @@
+module ModelParser
+  # todo how to use it???????
+  #   read about services
+
+  def parse_relations(file_path)
+    file = File.open(file_path, "r")
+    content = file.read
+    file.close
+    # binding.pry
+    # content.scan(/^ *(belongs_to.*|has_one.*)\r/).uniq
+
+    # add another checks
+    # add parameters check
+    {
+      belongs_to: parse_belongs_to(content),
+      has_many: parse_has_many(content),
+      has_one: parse_has_one(content),
+      has_and_belongs_to_many: parse_has_and_belongs_to_many(content)
+    }
+  end
+
+  private
+
+  def parse_belongs_to(content)
+    content.scan(/belongs_to\s:(\w+)/).flatten
+  end
+
+  def parse_has_many(content)
+    content.scan(/has_many\s:(\w+)/).flatten
+  end
+
+  def parse_has_one(content)
+    content.scan(/has_one\s:(\w+)/).flatten
+  end
+
+  def parse_has_and_belongs_to_many(content)
+    content.scan(/has_and_belongs_to_many\s:(\w+)/).flatten
+  end
+end
